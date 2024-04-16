352f26ab36f4c236cf825c760ec644a4798b1e58
Lyle Simmons <lylesimmons125@gmail.com>
2024-04-16 09:48:00 -0400

    lab complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens = [x for x in num_list if x % 2 == 0]
+    return evens
 
 def make_exclamation(sentence_list):
-    pass
+    new_list = [x + "!" for x in sentence_list]
+    return new_list
+    
